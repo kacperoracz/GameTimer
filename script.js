a1b33b1f38d2558542114d1afcaa01f69337e08d
Kacper Oracz <kacperoracz@gmail.com>
2022-04-10 17:28:25 +0200

    .

--- script.js
+++ script.js
@@ -16,8 +16,10 @@ const menu = document.querySelector(".menu");
 const menuHeader = document.querySelector(".menu h3");
 const playersButton = document.querySelector(".playersButton");
 const playersOptions = document.querySelector(".playersOptions");
+const playersOptionsBackButton = document.querySelector(".playersOptions .back");
 const timeButton = document.querySelector(".timeButton");
 const timeOptions = document.querySelector(".timeOptions");
+const timeOptionsBackButton = document.querySelector(".timeOptions .back");
 
 menuHeader.addEventListener("click", e => {
     menu.classList.toggle("open");
@@ -28,5 +30,13 @@ playersButton.addEventListener("click", e => {
 });
 
 timeButton.addEventListener("click", e => {
+    timeOptions.classList.toggle("open");
+});
+
+playersOptionsBackButton.addEventListener("click", e => {
+    playersOptions.classList.toggle("open");
+});
+
+timeOptionsBackButton.addEventListener("click", e => {
     timeOptions.classList.toggle("open");
 });

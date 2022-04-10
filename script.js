4a72becc464259b37fc0af64e5d994b6db538cdf
Kacper Oracz <kacperoracz@gmail.com>
2022-04-10 14:27:43 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// let mouseDown = 0;  
+// window.onmousedown = () => {  
+//   ++mouseDown;  
+//   if (mouseDown) {  
+//     console.log('mouse button down')  
+//   }  
+// }  
+// window.onmouseup = () => {  
+//   --mouseDown;  
+//   if (mouseDown) {  
+//     console.log('mouse button down')  
+//   }  
+// }
+
+// const menu = document.querySelector(".menu");
+// const menuHeader = document.querySelector(".menu h3");
+
+// menuHeader.addEventListener("click", e => {
+    
+// });
